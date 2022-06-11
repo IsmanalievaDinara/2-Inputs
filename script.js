1ea853971099b423658f6ff61d747c0886ccd402
Dinara <ismanalievadina@gmail.com>
2022-06-11 17:21:56 +0300

    2 Inputs

---
+++ script.js
@@ -0,0 +1,15 @@
+const firstNum = document.querySelector("#firstNum")
+const secondNum = document.querySelector("#secondNum")
+const button = document.querySelector("#sumBtn")
+const resultDiv = document.querySelector("#result")
+
+ function multiplyFunc(event){
+    let sum = firstNum.value * secondNum.value
+    resultDiv.innerHTML = sum
+ }
+ 
+
+
+
+
+ 
